b78e107b544229c14278a3907a6885387165c1fa
udonguk <udonguk@gmail.com>
2015-08-19 14:42:04 +0900

    git commit test jisong office

---
+++ src/net/donguk/test/HelloWorld.java
@@ -0,0 +1,14 @@
+package net.donguk.test;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * Created by jisong7 on 2015-08-19.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        String name = null;
+        System.out.println(StringUtils.isEmpty(name));
+    }
+}
